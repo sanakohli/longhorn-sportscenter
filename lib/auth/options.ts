a6287c812f836678aa77eacec883a641eea57c7c
sanakohli <kohlisana29@gmail.com>
2026-02-07 19:10:33 -0600

    Actually fixing routing

--- lib/auth/options.ts
+++ lib/auth/options.ts
@@ -16,6 +16,7 @@ declare module "next-auth" {
 }
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
+  trustHost: true,
   secret: process.env.NEXTAUTH_SECRET,
   providers: [
     Google({
